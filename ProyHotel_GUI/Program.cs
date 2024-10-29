480df183eb60793d25a3d4e5dcddf4d8159435b5
QuevedoJorge <jorgequeve.job@gmail.com>
2024-10-28 19:10:35 -0500

    fix: Inicio de aplicacion

--- ProyHotel_GUI/Program.cs
+++ ProyHotel_GUI/Program.cs
@@ -14,7 +14,7 @@ namespace ProyHotel_GUI
             Application.SetCompatibleTextRenderingDefault(false);
             //
             ApplicationConfiguration.Initialize();
-            Application.Run(new AgregarReservaHabitacion());
+            Application.Run(new Form1());
         }
     }
 }
