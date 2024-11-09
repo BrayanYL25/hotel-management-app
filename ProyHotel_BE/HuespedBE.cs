798e80cc8f85889bc8c0a1d1c2a4c1570f2b178b
QuevedoJorge <jorgequeve.job@gmail.com>
2024-11-09 13:32:36 -0500

    fix: Corrección de Views (tb_huesped)

--- ProyHotel_BE/HuespedBE.cs
+++ ProyHotel_BE/HuespedBE.cs
@@ -12,7 +12,10 @@ namespace ProyHotel_BE
         public String huesped_nombre { get; set; }
         public String huesped_telefono { get; set; }
         public String huesped_dni { get; set; }
+        public String huesped_correo { get; set; }
+        public Char huesped_sexo { get; set; }
         public DateTime fecha_creacion { get; set; }
         public DateTime fecha_ult_modificacion { get; set; }
+        public bool huesped_estado { get; set; }
     }
 }
--- ProyHotel_GUI/FrmHuespedes.Designer.cs
+++ ProyHotel_GUI/FrmHuespedes.Designer.cs
@@ -45,53 +45,49 @@
             dgtDatosHuesped.AllowUserToAddRows = false;
             dgtDatosHuesped.AllowUserToDeleteRows = false;
             dgtDatosHuesped.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            dgtDatosHuesped.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgtDatosHuesped.BackgroundColor = SystemColors.ControlLightLight;
             dgtDatosHuesped.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgtDatosHuesped.Location = new Point(12, 71);
-            dgtDatosHuesped.Margin = new Padding(3, 2, 3, 2);
+            dgtDatosHuesped.Location = new Point(14, 95);
             dgtDatosHuesped.Name = "dgtDatosHuesped";
             dgtDatosHuesped.ReadOnly = true;
             dgtDatosHuesped.RowHeadersVisible = false;
             dgtDatosHuesped.RowHeadersWidth = 51;
-            dgtDatosHuesped.Size = new Size(646, 265);
+            dgtDatosHuesped.Size = new Size(738, 353);
             dgtDatosHuesped.TabIndex = 1;
             // 
             // lblRegistroHuesped
             // 
             lblRegistroHuesped.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             lblRegistroHuesped.BorderStyle = BorderStyle.FixedSingle;
-            lblRegistroHuesped.Location = new Point(77, 361);
+            lblRegistroHuesped.Location = new Point(88, 481);
             lblRegistroHuesped.Name = "lblRegistroHuesped";
-            lblRegistroHuesped.Size = new Size(78, 22);
+            lblRegistroHuesped.Size = new Size(89, 29);
             lblRegistroHuesped.TabIndex = 2;
             // 
             // label2
             // 
             label2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             label2.AutoSize = true;
-            label2.Location = new Point(13, 362);
+            label2.Location = new Point(15, 483);
             label2.Name = "label2";
-            label2.Size = new Size(58, 15);
+            label2.Size = new Size(72, 20);
             label2.TabIndex = 0;
             label2.Text = "Cantidad:";
             // 
             // txtFiltro
             // 
-            txtFiltro.Location = new Point(419, 27);
-            txtFiltro.Margin = new Padding(3, 2, 3, 2);
+            txtFiltro.Location = new Point(479, 36);
             txtFiltro.Name = "txtFiltro";
             txtFiltro.PlaceholderText = "Ingrese el nombre";
-            txtFiltro.Size = new Size(152, 23);
+            txtFiltro.Size = new Size(173, 27);
             txtFiltro.TabIndex = 3;
             // 
             // btnAgregarHuesped
             // 
             btnAgregarHuesped.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            btnAgregarHuesped.Location = new Point(577, 363);
-            btnAgregarHuesped.Margin = new Padding(3, 2, 3, 2);
+            btnAgregarHuesped.Location = new Point(659, 484);
             btnAgregarHuesped.Name = "btnAgregarHuesped";
-            btnAgregarHuesped.Size = new Size(82, 25);
+            btnAgregarHuesped.Size = new Size(94, 33);
             btnAgregarHuesped.TabIndex = 4;
             btnAgregarHuesped.Text = "Agregar";
             btnAgregarHuesped.UseVisualStyleBackColor = true;
@@ -100,10 +96,9 @@
             // btnActualizarHuesped
             // 
             btnActualizarHuesped.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            btnActualizarHuesped.Location = new Point(489, 363);
-            btnActualizarHuesped.Margin = new Padding(3, 2, 3, 2);
+            btnActualizarHuesped.Location = new Point(559, 484);
             btnActualizarHuesped.Name = "btnActualizarHuesped";
-            btnActualizarHuesped.Size = new Size(82, 25);
+            btnActualizarHuesped.Size = new Size(94, 33);
             btnActualizarHuesped.TabIndex = 4;
             btnActualizarHuesped.Text = "Editar";
             btnActualizarHuesped.UseVisualStyleBackColor = true;
@@ -112,10 +107,9 @@
             // btnEliminarHuesped
             // 
             btnEliminarHuesped.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            btnEliminarHuesped.Location = new Point(401, 363);
-            btnEliminarHuesped.Margin = new Padding(3, 2, 3, 2);
+            btnEliminarHuesped.Location = new Point(458, 484);
             btnEliminarHuesped.Name = "btnEliminarHuesped";
-            btnEliminarHuesped.Size = new Size(82, 25);
+            btnEliminarHuesped.Size = new Size(94, 33);
             btnEliminarHuesped.TabIndex = 4;
             btnEliminarHuesped.Text = "Eliminar";
             btnEliminarHuesped.UseVisualStyleBackColor = true;
@@ -125,17 +119,18 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(13, 27);
+            label3.Location = new Point(15, 36);
             label3.Name = "label3";
-            label3.Size = new Size(178, 21);
+            label3.Size = new Size(221, 28);
             label3.TabIndex = 5;
             label3.Text = "Gestion de Huéspedes";
             // 
             // botonBuscar
             // 
-            botonBuscar.Location = new Point(583, 26);
+            botonBuscar.Location = new Point(666, 35);
+            botonBuscar.Margin = new Padding(3, 4, 3, 4);
             botonBuscar.Name = "botonBuscar";
-            botonBuscar.Size = new Size(75, 23);
+            botonBuscar.Size = new Size(86, 31);
             botonBuscar.TabIndex = 6;
             botonBuscar.Text = "Buscar";
             botonBuscar.UseVisualStyleBackColor = true;
@@ -143,9 +138,9 @@
             // 
             // FrmHuespedes
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(670, 399);
+            ClientSize = new Size(766, 532);
             Controls.Add(botonBuscar);
             Controls.Add(label3);
             Controls.Add(btnEliminarHuesped);
@@ -155,7 +150,6 @@
             Controls.Add(lblRegistroHuesped);
             Controls.Add(dgtDatosHuesped);
             Controls.Add(label2);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "FrmHuespedes";
             Text = "Gestion de Huespedes";
             Load += FrmHuespedes_Load;
