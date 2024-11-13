13e9401eeba258445f80156061645daf253d677e
QuevedoJorge <jorgequeve.job@gmail.com>
2024-11-13 00:25:58 -0500

    fix: Avance 2 - Edición de Reserva Habitación

--- ProyHotel_BE/ReservaHabitacionBE.cs
+++ ProyHotel_BE/ReservaHabitacionBE.cs
@@ -13,8 +13,8 @@ namespace ProyHotel_BE
         public string? tipoHabitacion { get; set; }
         public string? habitacionNombre { get; set; }
         public float costoNoche { get; set; }
-        public DateTime? fechaEntrada { get; set; }
-        public DateTime? fechaSalida { get; set; }
+        public DateTime fechaEntrada { get; set; }
+        public DateTime fechaSalida { get; set; }
         public float precioTotal { get; set; }
     }
 }
