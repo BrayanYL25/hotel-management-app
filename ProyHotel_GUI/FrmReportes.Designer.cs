3dd6335526f7e42a1a2389c15596d899250bd346
Brayan Yin Lin <brayanyl250206@gmail.com>
2024-11-26 01:37:51 -0500

    feat: generacion de reportes

--- ProyHotel_GUI/FrmReportes.Designer.cs
+++ ProyHotel_GUI/FrmReportes.Designer.cs
@@ -29,36 +29,62 @@
         private void InitializeComponent()
         {
             btn_Usuarios = new Button();
+            comboBoxPlantillas = new ComboBox();
+            label1 = new Label();
             SuspendLayout();
             // 
             // btn_Usuarios
             // 
-            btn_Usuarios.Font = new Font("Yu Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btn_Usuarios.Location = new Point(65, 95);
+            btn_Usuarios.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_Usuarios.Location = new Point(210, 117);
             btn_Usuarios.Name = "btn_Usuarios";
-            btn_Usuarios.Size = new Size(207, 72);
+            btn_Usuarios.Size = new Size(140, 39);
             btn_Usuarios.TabIndex = 0;
-            btn_Usuarios.Text = "Usuarios";
+            btn_Usuarios.Text = "Generar Reporte";
             btn_Usuarios.UseVisualStyleBackColor = true;
             btn_Usuarios.Click += btn_Usuarios_Click;
+            // 
+            // comboBoxPlantillas
+            // 
+            comboBoxPlantillas.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxPlantillas.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            comboBoxPlantillas.FormattingEnabled = true;
+            comboBoxPlantillas.Items.AddRange(new object[] { "Usuarios", "Habitacion por Reserva", "Servicios por Reserva", "Huespesdes en Habitacion por Reserva" });
+            comboBoxPlantillas.Location = new Point(12, 50);
+            comboBoxPlantillas.Name = "comboBoxPlantillas";
+            comboBoxPlantillas.Size = new Size(338, 29);
+            comboBoxPlantillas.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(157, 21);
+            label1.TabIndex = 2;
+            label1.Text = "Selecciona la tabla:";
             // 
             // FrmReportes
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(597, 521);
+            ClientSize = new Size(362, 168);
+            Controls.Add(label1);
+            Controls.Add(comboBoxPlantillas);
             Controls.Add(btn_Usuarios);
-            MaximizeBox = false;
-            MinimizeBox = false;
             Name = "FrmReportes";
             Text = "Reportes";
             Load += Reportes_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button btn_Usuarios;
+        private ComboBox comboBoxPlantillas;
+        private Label label1;
     }
 }
