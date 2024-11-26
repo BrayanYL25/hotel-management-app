c57633ce40cb69598ff9a73feb9754784ae40ee5
Brayan Yin Lin <106005521+BrayanYinLin@users.noreply.github.com>
2024-11-26 16:28:32 -0500

    Update Program.cs

--- ProyHotel_GUI/Program.cs
+++ ProyHotel_GUI/Program.cs
@@ -14,7 +14,7 @@ namespace ProyHotel_GUI
             Application.SetCompatibleTextRenderingDefault(false);
             //
             ApplicationConfiguration.Initialize();
-            Application.Run(new FrmUsuarios());
+            Application.Run(new FrmLogin());
         }
     }
 }
